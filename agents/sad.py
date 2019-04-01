620e4d6aae73ab74291fb4edf5eed2d146491f99
unknown <orthogonaltrajectory@gmail.com>
2019-04-01 12:34:45 -0800

    sad agent text noramalizers

--- agents/sad.py
+++ agents/sad.py
@@ -12,3 +12,13 @@ nltk.download('wordnet')
 
 corpusSentences = nltk.sent_tokenize(corpus)
 corpusWords = nltk.word_tokenize(corpus)
+
+# Tokenization and normalization globals
+lemmer = nltk.stem.WordNetLemmatizer()
+removePunctuation = dict((ord(punct), None) for punct in string.punctuation)
+
+def lemanizeTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+def normalize(text):
+    return lemanizeTokens(nltk.word_tokenize(text.lower().translate(removePunctuation)))
