eac73c9424273e685be0e68566b8d2c7b0c7a020
unknown <orthogonaltrajectory@gmail.com>
2019-04-11 11:33:23 -0800

    Sad agent branch

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from agent import Agent
 
 sadAgent = Agent(
     name = "Saddie", 
-    corpus = "sadPoems.txt", 
+    corpus = "sadQuotes.txt", 
     greetingMessage = "My name is Saddie The Sadbot. I'm an expert in sadness :(\nIf you want to leave, type 'bye'",
     defaultMessage = "Sorry.. I don't know what to say :(",
     goodbyeMessage = "Goodbye :("
